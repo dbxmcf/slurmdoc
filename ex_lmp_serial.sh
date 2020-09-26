54ffb212b63a011ded75d38b7c6067bc2a33969e
Feng Chen <fchen14@lsu.edu>
2020-09-26 12:56:53 -0500

    added serial lmp job example

---
+++ ex_lmp_serial.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=mpi_job_test      # Job name
+#SBATCH -p single                     # For jobs using more than 1 node, submit to workq
+#SBATCH --nodes=1                    # Number of nodes to be allocated
+#SBATCH --ntasks=1                  # Number of MPI tasks (i.e. processes/cores)
+#SBATCH --time=00:30:00              # Wall time limit (hh:mm:ss)
+#SBATCH --output=lmp_serial_%j.log     # Standard output and error 
+
+echo "Date              = $(date)"
+echo "Hostname          = $(hostname -s)"
+echo "Working Directory = $(pwd)"
+echo ""
+echo "Slurm Nodes Allocated          = $SLURM_JOB_NODELIST"
+echo "Number of Nodes Allocated      = $SLURM_JOB_NUM_NODES"
+echo "Number of Tasks Allocated      = $SLURM_NTASKS"
+
+module load mvapich2/2.3.3/intel-19.0.5
+module load lammps/20200303/intel-19.0.5-mvapich-2.3.3
+
+#srun -n $SLURM_NTASKS $(which lmp) -in in.lmp.lj 
+/usr/bin/time $(which lmp) -in in.lmp.ser.lj 
+
